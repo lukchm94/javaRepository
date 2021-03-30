72824b74e932ea7602c01dd48b144ad21c416f04
Lukasz Chmielewski <lukchm94@gmail.com>
2021-03-30 23:33:22 +0200

    created Java repo

---
+++ JavaFirst/src/com/example/test.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String name = "";
+        int n = 0;
+        double d = 0.0;
+    }
+}
