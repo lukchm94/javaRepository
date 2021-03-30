e98dcbf7d06d1981c293a44c3abc5a6cc1225bd3
Lukasz Chmielewski <lukchm94@gmail.com>
2021-03-30 23:37:00 +0200

    added first logic

--- JavaFirst/src/com/example/test.java
+++ JavaFirst/src/com/example/test.java
@@ -6,5 +6,12 @@ public class test {
         String name = "";
         int n = 0;
         double d = 0.0;
+        double sum = 0.0;
+
+        n = 1;
+        d = 20.2;
+        name = "5";
+
+        System.out.println(Integer.parseInt(name) + d);
     }
 }
